2a2ee3e98201ee5314c6dc8fa13f64a53a0a4236
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-07 21:04:45 +0600

    total student and available seat issue done

--- index.js
+++ index.js
@@ -60,7 +60,19 @@ async function run() {
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          isAdmin: true,
+          userRole: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          userRole: "insturctor",
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
@@ -79,6 +91,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/courses", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([]);
+      }
+      const query = { userEmail: email };
+      // console.log(query)
+      const result = await cartCollection.find(query).toArray();
+      // console.log(result)
+      res.send(result);
+    });
+
     app.post("/courses", async (req, res) => {
       const course = req.body;
       // console.log(course);
@@ -86,12 +111,49 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/courses/:id", async (req, res) => {
+    app.patch("/courses/approved/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          courseStatus: `admin`,
+          courseStatus: `approved`,
+        },
+      };
+      const result = await coursesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/courses/denied/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          courseStatus: `denied`,
+        },
+      };
+      const result = await coursesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/courses/updated/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      // Retrieve the current course document
+      const currentCourse = await coursesCollection.findOne(filter);
+
+      // Check if the course exists
+      if (!currentCourse) {
+        return res.status(404).send("Course not found");
+      }
+      // Calculate updated values
+      const updatedAvailableSeat = currentCourse.availableSeat - 1;
+      const updatedTotalStudent = currentCourse.totalStudent + 1;
+
+      const updateDoc = {
+        $set: {
+          availableSeat: updatedAvailableSeat,
+          totalStudent: updatedTotalStudent,
         },
       };
       const result = await coursesCollection.updateOne(filter, updateDoc);
